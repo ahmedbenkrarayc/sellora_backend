b7b2590966e5d013746545693555f663650be7c1
ahmedbenkrarayc <ahmed.benkrara2@gmail.com>
2025-03-24 23:32:40 +0000

    added relationshipstosubcategory model

--- app/Models/Subcategory.php
+++ app/Models/Subcategory.php
@@ -3,6 +3,8 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use App\Models\Category;
+use App\Models\Product;
 
 class Subcategory extends Model
 {
@@ -12,4 +14,12 @@ class Subcategory extends Model
         'status',
         'category_id'
     ];
+
+    public function category(){
+        return $this->belongsTo(Category::class, 'category_id');
+    }
+
+    public function products(){
+        return $this->hasMany(Product::class, 'subcategory_id');
+    }
 }
