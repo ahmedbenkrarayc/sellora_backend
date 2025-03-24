2b9400241b0155e327650c0c100b56b131792d75
ahmedbenkrarayc <ahmed.benkrara2@gmail.com>
2025-03-24 23:44:01 +0000

    added relationships to product model

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -4,6 +4,12 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
 
+use App\Models\Subcategory;
+use App\Models\Color;
+use App\Models\Size;
+use App\Models\ProductDetails;
+use App\Models\ProductVariant;
+
 class Product extends Model
 {
     protected $table = 'product';
@@ -13,4 +19,24 @@ class Product extends Model
         'baseprice',
         'subcategory_id'
     ];
+
+    public function subcategory(){
+        return $this->belongsTo(Subcategory::class, 'subcategory_id');
+    }
+
+    public function colors(){
+        return $this->hasMany(Color::class, 'product_id');
+    }
+
+    public function sizes(){
+        return $this->hasMany(Size::class, 'product_id');
+    }
+
+    public function productdetails(){
+        return $this->hasMany(ProductDetails::class, 'product_id');
+    }
+
+    public function productvariants(){
+        return $this->hasMany(ProductVariant::class, 'product_id');
+    }
 }
