a1b55962d3e0e9eb8fd94e0cbd7c7e25bb8e6cda
ahmedbenkrarayc <ahmed.benkrara2@gmail.com>
2025-03-24 14:10:48 +0000

    created order model

---
+++ app/Models/Order.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Order extends Model
+{
+    protected $table = 'order';
+    protected $fillable = [
+        'status',
+        'country',
+        'city',
+        'address',
+        'postalcode',
+        'price',
+        'customer_id'
+    ];
+}
