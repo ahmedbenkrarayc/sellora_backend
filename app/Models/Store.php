0f39c6b1b67e142f96b0084b260f3d6108cdf838
ahmedbenkrarayc <ahmed.benkrara2@gmail.com>
2025-03-24 12:43:07 +0000

    created store model

---
+++ app/Models/Store.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Store extends Model
+{
+    protected $table = 'store';
+    protected $fillable = [
+        'name',
+        'subdomain',
+        'domain',
+        'logo',
+        'description',
+        'type',
+        'status',
+        'storeowner_id'
+    ]
+}
