f0203ee5c58dc0e6c35208f7b2c67e227bd5a07c
ahmedbenkrarayc <ahmed.benkrara2@gmail.com>
2025-03-24 14:45:18 +0000

    added relationships to store model

--- app/Models/Store.php
+++ app/Models/Store.php
@@ -3,6 +3,9 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use App\Models\User;
+use App\Models\Customer;
+use App\Models\Category;
 
 class Store extends Model
 {
@@ -17,4 +20,16 @@ class Store extends Model
         'status',
         'storeowner_id'
     ];
+
+    public function customers(){
+        return $this->hasMany(Customer::class, 'store_id');
+    }
+
+    public function storeowner(){
+        return $this->belongsTo(User::class, 'storeowner_id');
+    }
+
+    public function categories(){
+        return $this->hasMany(Category::class, 'store_id');
+    }
 }
