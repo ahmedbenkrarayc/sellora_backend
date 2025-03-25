6fead0989e675263bb9064fa5b1dbb50b2e23a4a
ahmedbenkrarayc <ahmed.benkrara2@gmail.com>
2025-03-25 00:49:42 +0000

    added relationships to order model

--- app/Models/Order.php
+++ app/Models/Order.php
@@ -3,6 +3,9 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use App\Models\ProductVariant;
+use App\Models\Payment;
+use App\Models\Customer;
 
 class Order extends Model
 {
@@ -16,4 +19,17 @@ class Order extends Model
         'price',
         'customer_id'
     ];
+
+    public function productvariants(){
+        return $this->belongsToMany(ProductVariant::class, 'order_productvariant', 'order_id', 'productvariant_id')
+                ->withPivot('quantity');
+    }
+
+    public function payment(){
+        return $this->hasOne(Payment::class, 'order_id');
+    }
+
+    public function customer(){
+        return $this->belongsTo(Customer::class, 'customer_id');
+    }
 }
