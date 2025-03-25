139c8d4002564accc07f194cc347e5ba0197267d
ahmedbenkrarayc <ahmed.benkrara2@gmail.com>
2025-03-25 00:30:29 +0000

    added relationships to productvariant model

--- app/Models/ProductVariant.php
+++ app/Models/ProductVariant.php
@@ -4,6 +4,13 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
 
+use App\Models\Product;
+use App\Models\Color;
+use App\Models\Size;
+use App\Models\ProductVariantImage;
+use App\Models\Wishlist;
+use App\Models\Order;
+
 class ProductVariant extends Model
 {
     protected $table = 'productvariant';
@@ -16,4 +23,29 @@ class ProductVariant extends Model
         'color_id',
         'size_id'
     ];
+
+    public function product(){
+        return $this->belongsTo(Product::class, 'product_id');
+    }
+
+    public function color(){
+        return $this->belongsTo(Color::class, 'color_id');
+    }
+
+    public function size(){
+        return $this->belongsTo(Size::class, 'size_id');
+    }
+
+    public function images(){
+        return $this->hasMany(ProductVariantImage::class, 'productvariant_id');
+    }
+
+    public function wishlist(){
+        return $this->hasMany(Wishlist::class, 'productvariant_id');
+    }
+
+    public function orders(){
+        return $this->belongsToMany(Order::class, 'order_productvariant', 'productvariant_id', 'order_id')
+                ->withPivot('quantity');
+    }
 }
