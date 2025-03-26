274c9f591f55e3d9d29d326474db48991d87cdf3
ahmedbenkrarayc <ahmed.benkrara2@gmail.com>
2025-03-26 14:57:03 +0000

    created signature of updateResetToekn & updatePassword in authrepository contract

--- app/Repositories/Interfaces/IAuthRepository.php
+++ app/Repositories/Interfaces/IAuthRepository.php
@@ -7,4 +7,5 @@ interface IAuthRepository{
     public function createUser(array $data);
     public function findUserByEmail(string $email);
     public function updateResetToken(User $user, string $resetToken, string $expiresAt);
+    public function updatePassword(User $user, string $newPassword);
 }
