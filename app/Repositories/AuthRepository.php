01b3bf45d3e5966eb238ad027530ba9567c0607e
ahmedbenkrarayc <ahmed.benkrara2@gmail.com>
2025-03-26 11:25:35 +0000

    created updateResetToken method

--- app/Repositories/AuthRepository.php
+++ app/Repositories/AuthRepository.php
@@ -5,10 +5,9 @@ namespace App\Repositories;
 use App\Repositories\Interfaces\IAuthRepository;
 use App\Models\User;
 
-class AuthRepository implements AuthRepositoryInterface
+class AuthRepository implements IAuthRepository
 {
-    public function createUser(array $data)
-    {
+    public function createUser(array $data){
         return User::create([
             'fname' => $data['fname'],
             'lname' => $data['lname'],
@@ -19,8 +18,13 @@ class AuthRepository implements AuthRepositoryInterface
     }
 
     
-    public function findUserByEmail(string $email)
-    {
+    public function findUserByEmail(string $email){
         return User::where('email', $email)->first();
+    }
+
+    public function updateResetToken(User $user, string $resetToken, string $expiresAt){
+        $user->reset_token = $resetToken;
+        $user->reset_token_expires_at = $expiresAt;
+        return $user->save();
     }
 }
--- app/Repositories/Interfaces/IAuthRepository.php
+++ app/Repositories/Interfaces/IAuthRepository.php
@@ -1,8 +1,10 @@
 <?php
 
 namespace App\Repositories\Interfaces;
+use App\Models\User;
 
 interface IAuthRepository{
     public function createUser(array $data);
     public function findUserByEmail(string $email);
+    public function updateResetToken(User $user, string $resetToken, string $expiresAt);
 }
