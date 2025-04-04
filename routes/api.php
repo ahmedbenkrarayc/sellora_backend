97712ec5cbc8746ddee4d40724d86c484ca34b41
ahmedbenkrarayc <ahmed.benkrara2@gmail.com>
2025-04-04 03:58:53 +0000

    created store routes

--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,7 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Api\AuthController;
+use App\Http\Controllers\Api\StoreController;
 
 Route::post('/register', [AuthController::class, 'register']);
 Route::post('/login', [AuthController::class, 'login']);
@@ -13,4 +14,12 @@ Route::post('/reset-password', [AuthController::class, 'resetPassword']);
 Route::middleware(['jwt.api'])->group(function () {
     Route::get('/user', [AuthController::class, 'user']);
     Route::post('/logout', [AuthController::class, 'logout']);
+});
+
+Route::prefix('/stores')->group(function(){
+    Route::get('/{id}', [StoreController::class, 'show']);//everyone
+    Route::get('/', [StoreController::class, 'index'])->middleware(['jwt.api', 'role:storeowner']);
+    Route::post('/', [StoreController::class, 'store'])->middleware(['jwt.api', 'role:storeowner']);
+    Route::put('/{id}', [StoreController::class, 'update'])->middleware(['jwt.api', 'role:storeowner,superadmin']);
+    Route::delete('/{id}', [StoreController::class, 'destroy'])->middleware(['jwt.api', 'role:storeowner,superadmin']);
 });
