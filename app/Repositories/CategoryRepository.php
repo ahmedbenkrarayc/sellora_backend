301f5da03cb3aa2ade4186011dbe85c240c6daf0
ahmedbenkrarayc <ahmed.benkrara2@gmail.com>
2025-04-04 21:22:28 +0000

    created category repository

---
+++ app/Repositories/CategoryRepository.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\Repositories;
+
+use App\Models\Category;
+
+class CategoryRepository implements ICategoryRepository
+{
+    public function all()
+    {
+        return Category::all();
+    }
+
+    public function find(int $id)
+    {
+        return Category::find($id);
+    }
+
+    public function create(array $data)
+    {
+        return Category::create($data);
+    }
+
+    public function update(int $id, array $data)
+    {
+        $category = $this->find($id);
+        if ($category) {
+            return $category->update($data);
+        }
+        return false;
+    }
+
+    public function delete(int $id)
+    {
+        $category = $this->find($id);
+        if ($category) {
+            return $category->delete();
+        }
+        return false;
+    }
+}
