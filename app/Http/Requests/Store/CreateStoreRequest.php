3fffd131464a41b3eeb845d209578fb9a05fb666
ahmedbenkrarayc <ahmed.benkrara2@gmail.com>
2025-04-04 03:55:11 +0000

    created store create request

---
+++ app/Http/Requests/Store/CreateStoreRequest.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Http\Requests\Store;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class CreateStoreRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
+     */
+    public function rules(): array
+    {
+        return [
+            'name' => 'required|string|max:255',
+            'subdomain' => 'required|string|max:255|unique:store',
+            'domain' => 'nullable|string|max:255',
+            'logo' => 'nullable|image|mimes:jpeg,png,jpg,gif,svg|max:2048',
+            'description' => 'nullable|string',
+            'type' => 'required|string|max:50',
+            'status' => 'required|string|max:50',
+            'storeowner_id' => 'required|exists:users,id'
+        ];
+    }
+}
