16c270722bc5dc35f5b3deca16ed5dacab5ecd38
ahmedbenkrarayc <ahmed.benkrara2@gmail.com>
2025-04-06 20:39:25 +0100

    created wishlist controller

---
+++ app/Http/Controllers/Api/WishListController.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use App\Http\Requests\Wishlist\StoreWishlistRequest;
+use App\Services\WishlistService;
+
+class WishListController extends Controller
+{
+    private WishlistService $wishlistService;
+
+    public function __construct(WishlistService $wishlistService)
+    {
+        $this->wishlistService = $wishlistService;
+    }
+
+    public function index($id)
+    {
+        $wishlist = $this->wishlistService->getCustomerWishlist($id);
+
+        return response()->json($wishlist);
+    }
+
+    public function store(StoreWishlistRequest $request)
+    {
+        if($this->wishlistService->add($request->customer_id, $request->productvariant_id)){
+            return response()->json(['message' => 'Added to wishlist'], 201);
+        }
+
+        return response()->json(['message' => 'Something went wrong !'], 500);
+    }
+
+    public function destroy(Request $request, $id)
+    {
+        if($this->wishlistService->removeFromWishList($request->id)){
+            return response()->json([], 204);
+        }
+
+        return response()->json(['message' => 'WishList item not found!'], 204);
+    }
+}
