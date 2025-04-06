39162423e661b7890fee761d44ec8231ad8b947f
ahmedbenkrarayc <ahmed.benkrara2@gmail.com>
2025-04-06 21:21:19 +0100

    created order repository interface

---
+++ app/Repositories/Interfaces/IOrderRepository.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Repositories\Interfaces;
+
+interface IOrderRepository{
+    public function create(array $data);
+    public function findById(int $id);
+    public function updateStatus(int $id, string $status);
+    public function delete(int $id);
+    public function all();
+}
