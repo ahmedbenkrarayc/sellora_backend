d3e3f2d086599fd1e6c33e2668090c860e2535a5
ahmedbenkrarayc <ahmed.benkrara2@gmail.com>
2025-04-07 00:20:09 +0100

    created custom request for storing payment

---
+++ app/Http/Requests/Payment/StorePaymentRequest.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Http\Requests\Payment;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class StorePaymentRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
+     */
+    public function rules(): array
+    {
+        return [
+            'method' => ['required', 'string'],
+            'order_id' => ['required', 'exists:order,id'],
+        ];
+    }
+}
