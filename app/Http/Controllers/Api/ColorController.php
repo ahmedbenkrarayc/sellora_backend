a8b0d12431e6a2ba4dbf9d9b1fd2c9eda5f1d3b1
ahmedbenkrarayc <ahmed.benkrara2@gmail.com>
2025-04-06 03:24:32 +0100

    created controller for color

---
+++ app/Http/Controllers/Api/ColorController.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use App\Http\Requests\Color\StoreColorRequest;
+use App\Services\ColorService;
+
+class ColorController extends Controller
+{
+    private $colorService;
+
+    public function __construct(ColorService $colorService)
+    {
+        $this->colorService = $colorService;
+    }
+
+    public function store(StoreColorRequest $request)
+    {
+        $color = $this->colorService->create($request->validated());
+        return response()->json(['data' => $color, 'message' => 'Color created successfully.'], 201);
+    }
+
+    public function destroy(Request $request, $id)
+    {
+        $this->colorService->delete($id);
+        return response()->json(['message' => 'Color deleted successfully.']);
+    }
+
+    public function show($id)
+    {
+        $color = $this->colorService->show($id);
+        return response()->json(['data' => $color]);
+    }
+}
