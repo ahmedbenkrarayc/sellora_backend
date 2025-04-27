513bcbca81c26077fe8f18808c31b0c19764e668
ahmedbenkrarayc <ahmed.benkrara12@gmail.com>
2025-04-27 04:41:36 +0000

    integrated categoryresource with subcategory controller actions

--- app/Http/Controllers/Api/SubcategoryController.php
+++ app/Http/Controllers/Api/SubcategoryController.php
@@ -7,6 +7,7 @@ use Illuminate\Http\Request;
 use App\Http\Requests\Subcategory\StoreSubcategoryRequest;
 use App\Http\Requests\Subcategory\UpdateSubcategoryRequest;
 use App\Services\SubcategoryService;
+use App\Http\Resources\Category\SubCategoryResource;
 
 class SubcategoryController extends Controller
 {
@@ -19,20 +20,20 @@ class SubcategoryController extends Controller
 
     public function index()
     {
-        return response()->json($this->subcategoryService->getAll());
+        return SubcategoryResource::collection($this->subcategoryService->getAll());
     }
 
     public function store(StoreSubcategoryRequest $request)
     {
         $subcategory = $this->subcategoryService->create($request->validated());
-        return response()->json($subcategory, 201);
+        return (new SubcategoryResource($subcategory))->response()->setStatusCode(201);;
     }
 
     public function show($id)
     {
         $subcategory = $this->subcategoryService->getById($id);
         if($subcategory)
-            return response()->json($subcategory);
+            return new SubcategoryResource($subcategory);
         return response()->json(['message' => 'Subcategory not found']);
     }
 
