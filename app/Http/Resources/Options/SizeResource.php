ea00eca5d12097cc7010ca591df5f66efc3c5e99
ahmedbenkrarayc <ahmed.benkrara12@gmail.com>
2025-04-27 04:26:38 +0000

    created size resource

---
+++ app/Http/Resources/Options/SizeResource.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Http\Resources\Options;
+
+use Illuminate\Http\Request;
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class SizeResource extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @return array<string, mixed>
+     */
+    public function toArray(Request $request): array
+    {
+        return [
+            'id' => $this->id,
+            'name' => $this->name,
+            'product_id' => $this->product_id,
+        ];
+    }
+}
