6555f5f54e40f976cb1578fd9ade192d3fcfad52
ahmedbenkrarayc <ahmed.benkrara12@gmail.com>
2025-04-27 04:25:53 +0000

    created subcategory resource

---
+++ app/Http/Resources/Category/SubCategoryResource.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Http\Resources\Category;
+
+use Illuminate\Http\Request;
+use Illuminate\Http\Resources\Json\JsonResource;
+use App\Http\Resources\Category\CategoryResource;
+use App\Http\Resources\Product\ProductResource;
+
+class SubCategoryResource extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @return array<string, mixed>
+     */
+    public function toArray(Request $request): array
+    {
+        return [
+            'id' => $this->id,
+            'name' => $this->name,
+            'status' => $this->status,
+            'category_id' => $this->category_id,
+            'category' => new CategoryResource($this->whenLoaded('category')),
+            'products' => ProductResource::collection($this->whenLoaded('products')),
+        ];
+    }
+}
