4c7216eecda0c68feca1ea6279d209d2ca98e656
ahmedbenkrarayc <ahmed.benkrara12@gmail.com>
2025-05-04 20:26:16 +0000

    created event for broadcasting order statud update irt

---
+++ app/Events/RealTimeOrderStatusUpdated.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace App\Events;
+
+use Illuminate\Broadcasting\Channel;
+use Illuminate\Broadcasting\InteractsWithSockets;
+use Illuminate\Broadcasting\PresenceChannel;
+use Illuminate\Broadcasting\PrivateChannel;
+use Illuminate\Contracts\Broadcasting\ShouldBroadcast;
+use Illuminate\Foundation\Events\Dispatchable;
+use Illuminate\Queue\SerializesModels;
+use Illuminate\Contracts\Broadcasting\ShouldBroadcastNow;
+use App\Models\Order;
+
+class RealTimeOrderStatusUpdated implements ShouldBroadcastNow
+{
+    use Dispatchable, InteractsWithSockets, SerializesModels;
+
+    /**
+     * Create a new event instance.
+     */
+    public function __construct(public Order $order)
+    {
+        
+    }
+
+    /**
+     * Get the channels the event should broadcast on.
+     *
+     * @return array<int, \Illuminate\Broadcasting\Channel>
+     */
+    public function broadcastOn(): array
+    {
+        return [
+            new Channel('order-updated'.$this->order->customer->id),
+        ];
+    }
+
+    public function broadcastAs(): string
+    {
+        return 'order.updated';
+    }
+
+    public function broadcastWith(): array
+    {
+        return ['order' => $this->order];
+    }
+}
